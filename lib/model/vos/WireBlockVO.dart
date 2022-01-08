74bdae2a70401c18289cc33eaa43b293312d368d
Vladimir Cores (Minkin) <vladimir.minkin@gmail.com>
2022-01-08 17:35:00 +0300

    Calculate connection based on shortest distance between points

--- lib/model/vos/WireBlockVO.dart
+++ lib/model/vos/WireBlockVO.dart
@@ -2,17 +2,22 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 
-const BLOCK_WIDTH = 140.0;
-const BLOCK_HEIGHT = 100.0;
-
 class WireBlockVO {
   final MutableRectangle<double> block;
   List<WireBlockVO>? connections;
+  List<Point<double>> connectionPoints = [];
 
-  late final ValueNotifier<Point<double>> position;
+  _calculateConnectionPoints() {
+    connectionPoints = [
+      Point<double>(block.left + block.width / 2,   block.top), // TOP
+      Point<double>(block.left + block.width,       block.top + block.height / 2), // RIGHT
+      Point<double>(block.left + block.width / 2,   block.bottom), // BOTTOM
+      Point<double>(block.left,                     block.top + block.height / 2) // LEFT
+    ];
+  }
 
   WireBlockVO(this.block, { this.connections }) {
-    position = ValueNotifier<Point<double>>(block.topLeft);
+    _calculateConnectionPoints();
   }
 
   factory WireBlockVO.fromPosition(Point<double> position, { connections }) {
@@ -20,25 +25,23 @@ class WireBlockVO {
       MutableRectangle<double>(
         position.x,
         position.y,
-        BLOCK_WIDTH,
-        BLOCK_HEIGHT
+        160.0,
+        120.0
       ),
       connections: connections);
   }
 
-  get x => block.left - block.width / 2;
-  get y => block.top - block.height / 2;
+  get x => block.left;
+  get y => block.top;
+
   get top => y;
   get bottom => y + block.height;
   get left => x;
   get right => x + block.width;
 
-  get topLeft => Point<double>(left, top);
-  get bottomRight => Point<double>(right, bottom);
-
   set offset(Offset value) {
     block.left += value.dx;
     block.top += value.dy;
-    position.value = block.topLeft;
+    _calculateConnectionPoints();
   }
 }
--- lib/view/components/painter/WiresPainterWidget.dart
+++ lib/view/components/painter/WiresPainterWidget.dart
@@ -23,7 +23,7 @@ class WiresPainterWidget extends StatelessWidget {
 class PathPainter extends CustomPainter {
   final WireBlockVO wireBlockVO;
 
-  PathPainter(this.wireBlockVO):super(repaint: wireBlockVO.position) {}
+  PathPainter(this.wireBlockVO):super();
 
   void _drawDot(Canvas canvas, Point<double> position, [color = Colors.red]) => canvas.drawCircle(
     Offset(position.x, position.y), 4, Paint()..color = color);
@@ -32,42 +32,36 @@ class PathPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     if (wireBlockVO.connections == null) return;
 
-    final Point<double> topLeft = PositionUtils.snapToGridPoint(wireBlockVO.topLeft);
-    final Point<double> bottomRight = PositionUtils.snapToGridPoint(wireBlockVO.bottomRight);
-    final Point<double> center = Point((topLeft.x + bottomRight.x) / 2, (topLeft.y + bottomRight.y) / 2);
-
     // _drawDot(canvas, center);
     // _drawDot(canvas, topLeft, Colors.pink);
+    final connectionPoints = wireBlockVO.connectionPoints;
 
-    // List<CubicBezier> curves = wireBlockVO.connections!
     wireBlockVO.connections!
       .where((WireBlockVO c) => !c.block.intersects(wireBlockVO.block))
       .map((WireBlockVO c) {
 
-      final Point<double> blockTopLeft = PositionUtils.snapToGridPoint(c.topLeft);
-      final Point<double> blockBottomRight = PositionUtils.snapToGridPoint(c.bottomRight);
-      final Point<double> blockCenter = Point((blockTopLeft.x + blockBottomRight.x) / 2, (blockTopLeft.y + blockBottomRight.y) / 2);
+      Point<double> pOne = connectionPoints[0];
+      Point<double> pTwo = c.connectionPoints[0];
+      double
+        minDistance = double.infinity,
+        testDistance = double.infinity;
 
-      // _drawDot(canvas, blockCenter);
-      // _drawDot(canvas, blockTopLeft, Colors.pink);
-      // _drawDot(canvas, blockBottomRight, Colors.blue);
-      
-      final blockFromLeft = blockCenter.x < topLeft.x;
-      final blockFromTop = blockBottomRight.y < topLeft.y;
-      final blockFromBottom = blockTopLeft.y > bottomRight.y;
-      final blockInsideWidth = blockCenter.x > topLeft.x && blockCenter.x < bottomRight.x;
+      connectionPoints.forEach((p1) {
+        c.connectionPoints.forEach((p2) {
+          testDistance = p1.distanceTo(p2);
+          if (testDistance < minDistance) {
+            pOne = p1;
+            pTwo = p2;
+            minDistance = testDistance;
+          }
+        });
+      });
 
-      Offset pStart = Offset(blockFromLeft ? topLeft.x : bottomRight.x, center.y);
-      Offset pEnd = Offset(blockFromLeft ? blockBottomRight.x : blockTopLeft.x, blockCenter.y);
+      pOne = PositionUtils.snapToGridPoint(pOne);
+      pTwo = PositionUtils.snapToGridPoint(pTwo);
 
-      if (blockFromTop) {
-        pEnd = Offset(blockCenter.x, blockBottomRight.y);
-        if (blockInsideWidth) pStart = Offset(center.x, topLeft.y);
-      }
-      else if (blockFromBottom) {
-        pEnd = Offset(blockCenter.x, blockTopLeft.y);
-        if (blockInsideWidth) pStart = Offset(center.x, bottomRight.y);
-      }
+      Offset pStart = Offset(pOne.x, pOne.y);
+      Offset pEnd = Offset(pTwo.x, pTwo.y);
 
       canvas.drawPoints(ui.PointMode.points, [ pStart, pEnd ], Paint()
         ..color = Colors.black
@@ -80,46 +74,7 @@ class PathPainter extends CustomPainter {
         ..style = PaintingStyle.stroke
         ..strokeWidth = 2
       );
-
-      // Point<double> pOne = Point(
-      //   blockInsideWidth ? pStart.x : (pStart.x + pEnd.x) / 2,
-      //   blockFromTop ? (pStart.y + pEnd.y) / 2 : pStart.y
-      // );
-      // Point<double> pTwo = Point(
-      //   blockFromTop ? pEnd.x : (pStart.x + pEnd.x) / 2,
-      //   blockFromBottom ? (pStart.y + pEnd.y) / 2 : pEnd.y
-      // );
-
-      // return CubicBezier([
-      //   math.Vector2(pStart.x, pStart.y),
-      //   math.Vector2(pOne.x, pOne.y),
-      //   math.Vector2(pTwo.x, pTwo.y),
-      //   math.Vector2(pEnd.x, pEnd.y),
-      // ]);
     }).toList();
-
-    // curves.forEach((curve) {
-    //   final points = curve.points;
-    //   canvas.drawPoints(ui.PointMode.points,
-    //     points
-    //       // .where((p) => [0, 3].contains(points.indexOf(p)))
-    //       .map((p) => Offset(p.x, p.y)).toList(),
-    //     Paint()
-    //       ..color = Colors.black
-    //       ..strokeWidth = 10
-    //       ..strokeCap = StrokeCap.round
-    //   );
-    //
-    //   final path = Path()
-    //     ..moveTo(points.first.x, points.first.y)
-    //     ..cubicTo(points[1].x, points[1].y, points[2].x, points[2].y, points.last.x, points.last.y);
-    //   canvas.drawPath(path,
-    //     Paint()
-    //       ..color = Colors.black
-    //       ..style = PaintingStyle.stroke
-    //       ..strokeWidth = 2
-    //   );
-    // });
   }
 
   @override
